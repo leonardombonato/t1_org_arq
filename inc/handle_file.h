cbf8768dbd9df4fea81d6f8f00760decc1a130ab
Paulo Ricardo <pauloricardojm@hotmail.com.br>
2018-04-17 14:37:16 -0300

    Add deletar registro por rrn

--- inc/handle_file.h
+++ inc/handle_file.h
@@ -3,6 +3,7 @@
         #include "escola.h"
 
         void file_read_csv_write_binary(const char *nome_arq_dados, const char *nome_arq_binario);
+        void file_delete_record(const char *nome_arq_binario, int rrn);
         ESCOLA *file_read_binary_rrn(const char *nome_arq_binario, const int rrn);
         ESCOLA *file_read_all_binary(const char *nome_arq_binario);
 #endif
