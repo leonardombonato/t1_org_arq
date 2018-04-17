b9428f76192e0aa39d71717708e0169d1031af65
Paulo Ricardo <pauloricardojm@hotmail.com.br>
2018-04-17 14:34:43 -0300

    Add ler por rrn e ler tudo

--- inc/handle_file.h
+++ inc/handle_file.h
@@ -1,5 +1,8 @@
 #ifndef HANDLE_FILE_H
     #define HANDLE_FILE_H
+        #include "escola.h"
 
-        int file_read_csv_write_binary(const char *nome_arquivo, const char *binary_file);
+        void file_read_csv_write_binary(const char *nome_arq_dados, const char *nome_arq_binario);
+        ESCOLA *file_read_binary_rrn(const char *nome_arq_binario, const int rrn);
+        ESCOLA *file_read_all_binary(const char *nome_arq_binario);
 #endif
