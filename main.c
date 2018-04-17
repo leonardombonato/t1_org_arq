5f933d9c628ac7819b8d43043fde5135bda0659d
Paulo Ricardo <pauloricardojm@hotmail.com.br>
2018-04-16 22:21:02 -0300

    Arquivo principal

--- main.c
+++ main.c
@@ -1,5 +1,14 @@
+/*
+	Nome: André, Nº USP:
+	Nome: Francisco, Nº USP:
+	Nome: Leonardo, Nº USP:
+	Nome: Paulo Ricardo J. Miranda, Nº USP: 10133456
+*/
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include "inc/escola.h"
+#include "inc/handle_file.h"
 
 int main(int argc, char *argv[])
 {
