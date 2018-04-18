fe556a3d91fba1066411255959c01915c2b3d8e2
Paulo Ricardo <pauloricardojm@hotmail.com.br>
2018-04-18 10:33:49 -0300

    Add funcao para imprimir pilha

--- inc/handle_file.h
+++ inc/handle_file.h
@@ -4,6 +4,7 @@
 
         void file_read_csv_write_binary(const char *nome_arq_dados, const char *nome_arq_binario);
         void file_delete_record(const char *nome_arq_binario, int rrn);
+        void file_print_stack(const char *nome_arq_binario);
         ESCOLA *file_read_binary_rrn(const char *nome_arq_binario, const int rrn);
         ESCOLA *file_read_all_binary(const char *nome_arq_binario);
 #endif
