b2f3ea7dd1a03b2c7e5d222ed9e35022f2d5715a
Paulo Ricardo <pauloricardojm@hotmail.com.br>
2018-04-23 20:57:15 -0300

    Add alguns comentarios

--- inc/handle_file.h
+++ inc/handle_file.h
@@ -2,9 +2,13 @@
     #define HANDLE_FILE_H
         #include "escola.h"
 
+        // funcionalidade 1
         void file_read_csv_write_binary(const char *nome_arq_dados, const char *nome_arq_binario); // funcao para ler do csv e escrever no arquivo em binario
-        void file_delete_record(const char *nome_arq_binario, int rrn); // funcao para remover um registro dado seu RRN
-        void file_print_stack(const char *nome_arq_binario); // funcao para imprimir a pilha
+        // funcionalidade 4
         ESCOLA *file_read_binary_rrn(const char *nome_arq_binario, const int rrn); // funcao para buscar um registro dado seu RRN
+        // funcionalidade 5
+        void file_delete_record(const char *nome_arq_binario, int rrn); // funcao para remover um registro dado seu RRN
+        // funcionalidade 9
+        void file_print_stack(const char *nome_arq_binario); // funcao para imprimir a pilha
         ESCOLA *file_read_all_binary(const char *nome_arq_binario); // funcao para recuperar todos os registros do arquivo binario
 #endif
